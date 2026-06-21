6b0b42ad2eaa4c76a0d7e6ff3fe741eae8ab2193
Beryth <niggerimposant@gmail.com>
2026-06-21 08:19:47 +0200

    Implement background script for Chrome extension

---
+++ src/background/index.ts
@@ -0,0 +1,29 @@
+import { RulesManager } from "./rulesManager";
+import { resolveConfigForOrigin } from "./configResolver";
+import { applyHeaderRules } from "./headerMutator";
+import { StoragePolicy } from "./storagePolicy";
+
+const rules = new RulesManager();
+const storagePolicy = new StoragePolicy();
+
+chrome.runtime.onInstalled.addListener(async () => {
+  await rules.bootstrap();
+  await applyHeaderRules(rules);
+});
+
+chrome.runtime.onStartup.addListener(async () => {
+  await rules.refreshIfStale();
+  await applyHeaderRules(rules);
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg?.type === "GET_CONFIG_FOR_ORIGIN") {
+    resolveConfigForOrigin(msg.origin).then(sendResponse);
+    return true; 
+  }
+  return false;
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  storagePolicy.onTabClosed(tabId);
+});
