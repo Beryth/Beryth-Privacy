3bbfec08cd46120d88ae72c16df6e505a38b32bf
Beryth <niggerimposant@gmail.com>
2026-06-21 10:27:31 +0200

    Refactor header rules to use declarativeNetRequest API

--- packages/browser-extension/src/background/headerMutator.ts
+++ packages/browser-extension/src/background/headerMutator.ts
@@ -1,6 +1,8 @@
 import type { RulesManager } from "./rulesManager";
 import { activeProfile } from "./configResolver";
 
+const dnr = chrome.declarativeNetRequest;
+
 export async function applyHeaderRules(_rules: RulesManager): Promise<void> {
   const p = activeProfile();
 
@@ -9,43 +11,43 @@ export async function applyHeaderRules(_rules: RulesManager): Promise<void> {
       id: 1001,
       priority: 1,
       action: {
-        type: "modifyHeaders",
+        type: dnr.RuleActionType.MODIFY_HEADERS,
         requestHeaders: [
-          { header: "user-agent", operation: "set", value: p.userAgent },
+          { header: "user-agent", operation: dnr.HeaderOperation.SET, value: p.userAgent },
           {
             header: "accept-language",
-            operation: "set",
+            operation: dnr.HeaderOperation.SET,
             value: p.acceptLanguage,
           },
-          { header: "sec-ch-ua", operation: "set", value: p.secChUa },
-          { header: "sec-ch-ua-platform", operation: "set", value: p.secChUaPlatform },
-          { header: "sec-ch-ua-mobile", operation: "set", value: "?0" },
-          { header: "sec-ch-ua-full-version-list", operation: "remove" },
-          { header: "sec-ch-ua-arch", operation: "remove" },
-          { header: "sec-ch-ua-model", operation: "remove" },
-          { header: "sec-ch-ua-bitness", operation: "remove" },
-          { header: "dnt", operation: "remove" },
+          { header: "sec-ch-ua", operation: dnr.HeaderOperation.SET, value: p.secChUa },
+          { header: "sec-ch-ua-platform", operation: dnr.HeaderOperation.SET, value: p.secChUaPlatform },
+          { header: "sec-ch-ua-mobile", operation: dnr.HeaderOperation.SET, value: "?0" },
+          { header: "sec-ch-ua-full-version-list", operation: dnr.HeaderOperation.REMOVE },
+          { header: "sec-ch-ua-arch", operation: dnr.HeaderOperation.REMOVE },
+          { header: "sec-ch-ua-model", operation: dnr.HeaderOperation.REMOVE },
+          { header: "sec-ch-ua-bitness", operation: dnr.HeaderOperation.REMOVE },
+          { header: "dnt", operation: dnr.HeaderOperation.REMOVE },
         ],
       },
       condition: {
         urlFilter: "*",
         resourceTypes: [
-          "main_frame",
-          "sub_frame",
-          "xmlhttprequest",
-          "script",
-          "image",
-          "stylesheet",
-          "font",
-          "media",
-          "websocket",
-          "other",
+          dnr.ResourceType.MAIN_FRAME,
+          dnr.ResourceType.SUB_FRAME,
+          dnr.ResourceType.XMLHTTPREQUEST,
+          dnr.ResourceType.SCRIPT,
+          dnr.ResourceType.IMAGE,
+          dnr.ResourceType.STYLESHEET,
+          dnr.ResourceType.FONT,
+          dnr.ResourceType.MEDIA,
+          dnr.ResourceType.WEBSOCKET,
+          dnr.ResourceType.OTHER,
         ],
       },
     },
   ];
 
-  await chrome.declarativeNetRequest.updateDynamicRules({
+  await dnr.updateDynamicRules({
     removeRuleIds: headerRules.map((r) => r.id),
     addRules: headerRules,
   });
